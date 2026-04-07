2d756f6096f39bdf07fe24a9c45c280a7b2fd8af
rafik <rafikg322@gmail.com>
2026-04-07 23:52:47 +0100

    Fixed strings.xml Added Function To login button

--- app/src/main/java/com/example/todolist3/MainActivity.java
+++ app/src/main/java/com/example/todolist3/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.todolist3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        username = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword2);
+        loginButton = findViewById(R.id.LoginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                   // kma yzid zaka page jdida ndiro navigate
+                }
+            }
+        });
+
+
     }
 }
